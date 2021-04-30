b16e6a510eb054c6eef625ef049b55946d3f7052
ryulurala <ryulurala@gmail.com>
2021-04-30 20:02:13 +0900

    Add code: makeId()
    Generate randome id

---
+++ server/src/utils/util.js
@@ -0,0 +1,13 @@
+const makeId = (characters, length) => {
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    const randIdx = Math.floor(Math.random() * characters.length);
+    result += characters.charAt(randIdx);
+  }
+
+  return result;
+};
+
+module.exports = {
+  makeId,
+};
