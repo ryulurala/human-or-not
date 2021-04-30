7138d417d2c6a229d23ebf40736f221d6569fb87
ryulurala <ryulurala@gmail.com>
2021-04-30 20:05:46 +0900

    Refactor: gather packet definition in one place
    ++ define new Packet

--- server/src/packet/index.js
+++ server/src/packet/index.js
@@ -1,11 +1,9 @@
-const serverPacket = require("./server-packet");
-const clientPacket = require("./client-packet");
+const Packet = require("./packet");
 
 module.exports = {
   /**
-  @TODO
-  packetManager,
-   **/
-  serverPacket,
-  clientPacket,
+   * @TODO
+   * packetManager,
+   */
+  Packet,
 };
--- server/src/packet/packet.js
+++ server/src/packet/packet.js
@@ -6,7 +6,48 @@ class Packet {
   }
 }
 
+class C_CreateRoom extends Packet {
+  constructor() {
+    super(PACKET_ID.C_CreateRoom);
+  }
+}
+
+class C_EnterRoom extends Packet {
+  constructor(roomId) {
+    super(PACKET_ID.C_EnterRoom);
+
+    this.roomId = roomId;
+  }
+}
+
+class S_BroadcastEnterRoom extends Packet {
+  constructor(playerId) {
+    super(PACKET_ID.S_BroadcastEnterRoom);
+
+    this.playerId = playerId;
+  }
+}
+
+class S_BroadcastLeaveRoom extends Packet {
+  constructor(playerId) {
+    super(PACKET_ID.S_BroadcastLeaveRoom);
+
+    this.playerId = playerId;
+  }
+}
+
+class S_PlayerList extends Packet {
+  constructor(playerId) {
+    super(PACKET_ID.S_PlayerList);
+
+    this.players = [];
+  }
+}
+
 module.exports = {
-  PACKET_ID,
-  Packet,
+  C_CreateRoom,
+  C_EnterRoom,
+  S_BroadcastEnterRoom,
+  S_BroadcastLeaveRoom,
+  S_PlayerList,
 };
