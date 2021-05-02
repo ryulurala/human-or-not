ca27c33ca6a9c2bb28ce65b21b65a7bece1b48b3
ryulurala <ryulurala@gmail.com>
2021-05-02 20:09:19 +0900

    Update index.js in packet directory:
    Lobby Packet Manager와 Game Packet Manager의 각기 다른 핸들러 등록 및
    export

--- server/src/packet/index.js
+++ server/src/packet/index.js
@@ -1,9 +1,33 @@
-const Packet = require("./packet");
+const PACKET_ID = require("./pakcet-list.json");
+const PacketManager = require("./packet-manager");
+const PacketHandler = require("./packet-handler");
+
+const initLobbyPacket = () => {
+  const lobbyPacketManager = new PacketManager();
+
+  lobbyPacketManager.Register(
+    PACKET_ID.C_CreateRoom,
+    PacketHandler.C_CreateRoomHandler
+  );
+
+  lobbyPacketManager.Register(
+    PACKET_ID.C_EnterRoom,
+    PacketHandler.C_EnterRoomHandler
+  );
+
+  return lobbyPacketManager;
+};
+
+const initGamePacket = () => {
+  const gamePacketManager = new PacketManager();
+
+  return gamePacketManager;
+};
+
+const lobbyPacketManager = initLobbyPacket();
+const gamePacketManager = initGamePacket();
 
 module.exports = {
-  /**
-   * @TODO
-   * packetManager,
-   */
-  Packet,
+  lobbyPacketManager,
+  gamePacketManager,
 };
